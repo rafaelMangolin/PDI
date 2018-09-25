a97ea7d367383b7bb0d0a64e768a9805b2db76da
Rafael Mangolin <rbmangolin@gmail.com>
2018-09-25 18:20:19 -0300

    Finalizado

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
+import cv2
 import numpy as np
 
-a = np.array([[1,2,3],[4,5,6]])
+a = cv2.imread('beico.jpeg')
+b = cv2.imread('4:1:1_beico.jpeg')
+c = cv2.imread('4:2:0_beico.jpeg')
+d = cv2.imread('4:2:2_beico.jpeg')
+e = cv2.imread('4:4:4_beico.jpeg')
+	
+ab = a == b
 
-b = np.repeat(a,2,axis=(0,1))
 
-# c = np.repeat(b,2,axis=0)
+	
+def mse(imageA, imageB):
+	# the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+	
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
 
-print(a)
-print(b)
-print(c)
+
+print(mse(a,b))
+print(mse(a,c))
+print(mse(a,d))
+print(mse(a,e))
+# print(b)
+# print(c)
+
+
+
+
